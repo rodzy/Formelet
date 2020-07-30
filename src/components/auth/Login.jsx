44751c4fe1d03780eb702e496ef81086684ace4c
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-29 20:27:21 -0600

    v.1 form validation ✅

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import { Heading } from "../styled-components/Heading";
 import { MainContainer } from "../styled-components/MainContainer";
 import { HeadingContainer } from "../styled-components/HeadingContainer";
@@ -9,20 +9,66 @@ import { Form } from "../styled-components/Form";
 import { FormContent } from "../styled-components/FormContent";
 
 const Login = () => {
+  //State
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(false);
+
+  //Constants
+  const { email, password } = user;
+
+  //Events
+  const onChange = (e) => {
+    setUser({
+      ...user,
+      [e.currentTarget.name]: e.currentTarget.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (email.trim() === "" || password.trim() === "") {
+      setError(true);
+      return;
+    }
+  };
+
   return (
     <Fragment>
       <MainContainer>
         <HeadingContainer>
           <Heading>Log in</Heading>
         </HeadingContainer>
-        <Form>
+        <Form onSubmit={onSubmit}>
           <FormContent>
-            <Labels>Email</Labels>
-            <Inputs/>   
-            <Labels>Password</Labels>
-            <Inputs/>
+            <Labels htmlFor="email">Email</Labels>
+            <Inputs
+              type="email"
+              id="email"
+              name="email"           
+              value={email}
+              onChange={onChange}
+            />
+            <Labels htmlFor="password">Password</Labels>
+            <Inputs
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+            />
+            <h4
+              style={{
+                color: `red`,
+                textAlign:`center`
+              }}
+            >
+              {error ? "All the fields are required!" : null}
+            </h4>
           </FormContent>
-          <Button>Log in</Button>
+          <Button type="submit">Log in</Button>
         </Form>
       </MainContainer>
     </Fragment>
