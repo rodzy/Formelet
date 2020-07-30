d8e2c454a0164eb48df94077de7d6a14029b5ec9
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-29 18:41:26 -0600

    Finished form styling

--- src/components/styled-components/Labels.jsx
+++ src/components/styled-components/Labels.jsx
@@ -2,11 +2,11 @@ import styled from "@emotion/styled";
 
 export const Labels = styled.label`
   height: 12px;
-  color:#A6E7FF;
+  color:#5327CB;
   text-shadow: 3px 3px #DEE6ED;
   font-family: Roboto, "Helvetica Neue", sans-serif;
-  font-size: 17px;
-  font-weight: 600;
+  font-size: 19px;
+  font-weight: 800;
   letter-spacing: 0.5px;
   line-height: 13px;
   float: left;
