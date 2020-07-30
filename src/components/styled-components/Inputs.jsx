429259ccae3b51ee30ca600094b43cbf19a4bba2
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-29 20:27:21 -0600

    v.1 form validation ✅

--- src/components/styled-components/Inputs.jsx
+++ src/components/styled-components/Inputs.jsx
@@ -9,9 +9,10 @@ export const Inputs = styled.input`
   border-radius: 4px;
   background-color: #E6E6FA;
   font-family: Roboto, "Helvetica Neue", sans-serif;
-  font-size: 14px;
+  font-size: 18px;
+  font-weight:600;
   line-height: 15px;
   text-indent: 16px;
-  color: #ffffff;
+  color:#5327CB;
 
 `;
