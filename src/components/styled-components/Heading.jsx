97e0310c9bbe742055e2e42f8661d49ced1cfe21
rodzy <49137701+rodzy@users.noreply.github.com>
2020-07-29 18:00:51 -0600

    Heading styling

--- src/components/styled-components/Heading.jsx
+++ src/components/styled-components/Heading.jsx
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled'
 
 export const Heading=styled.h1`
-    color:red;
+    color:#ffff;
+    border:3px;
+    border-color:#000000;
+    font-weight:800;
+    text-shadow:3px 4px #000000;
 `
---
+++ src/components/styled-components/HeadingContainer.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+export const HeadingContainer = styled.div`
+  margin: 48px auto 32px;
+  height: 56px;
+  font-weight: 600;
+  text-shadow: 3px 3px #3c045c;
+  font-family: Roboto, "Helvetica Neue", sans-serif;
+  font-size: 18px;
+  letter-spacing: -0.5px;
+  line-height: 64px;
+  text-align: center;
+  font-stretch: normal;
+`;
